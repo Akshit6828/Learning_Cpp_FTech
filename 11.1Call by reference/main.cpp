0a880b6e74046cca5f711fa2e62a1bacc1b6982c
Akshit Mangotra <57808363+Akshit6828@users.noreply.github.com>
2021-07-06 04:42:56 +0530

    Added Call By reference for multiply

---
+++ 11.1Call by reference/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+void areaperimeter(int,float *a,float *p);
+
+int main()
+{
+    int radius;
+    int x;
+    float area,perimeter;
+    cout<<"Enter radius";
+    cin>>radius;
+    areaperimeter(radius,area *,perimeter *);
+    cout<<"area ="<<area;
+    cout<<"Perimeter ="<<perimeter;
+   cout<<x;
+
+    return 0;
+}
+void areaperimeter(int,float *a,float *p)
+{
+    *a=3.14*r*r;
+    *p=2*3.14*r;
+
+}
