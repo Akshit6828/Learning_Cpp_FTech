c230092789f0f6aa06865d56e0a4e39b16122a9d
Akshit Mangotra <akshitmangotra@gmail.com>
2021-07-10 23:29:51 +0530

    Formated Repository

---
+++ 32 Range_Datatypes_limits/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<limits.h>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{   cout<<endl<<endl<<endl;
+    printf("The no of BITS in a BYTE is %d\n",CHAR_BIT);
+     printf("The minimum value of SIGNED CHAR = %d\n", SCHAR_MIN);
+   printf("The maximum value of SIGNED CHAR = %d\n", SCHAR_MAX);
+   printf("The maximum value of UNSIGNED CHAR = %d\n", UCHAR_MAX);
+
+   printf("The minimum value of SHORT INT = %d\n", SHRT_MIN);
+   printf("The maximum value of SHORT INT = %d\n", SHRT_MAX);
+
+   printf("The minimum value of INT = %d\n", INT_MIN);
+   printf("The maximum value of INT = %d\n", INT_MAX);
+
+   printf("The minimum value of CHAR = %d\n", CHAR_MIN);
+   printf("The maximum value of CHAR = %d\n", CHAR_MAX);
+
+   printf("The minimum value of LONG = %ld\n", LONG_MIN);
+   printf("The maximum value of LONG = %ld\n", LONG_MAX);
+
+    return 0;
+}
---
+++ MIX_QUESTIONS/ATM Problem CodeChef/main.cpp
@@ -0,0 +1,38 @@
+// CC Problem URL: https://www.codechef.com/problems/HS08TEST
+#include <iostream>
+#include<iomanip>
+#include<stdlib.h>
+using namespace std;
+
+int main() {
+	 int X;
+	 if(X<0)
+     {
+
+         X=-1*X;
+     }
+	 float Y;
+	 if(Y<0)
+     {
+         Y=-1*Y;
+     }
+	cin>>X; cin>>Y;
+     if(Y<0)
+	 {
+	     Y = -Y;
+	 }
+	if(X%5==0&&X+0.50<=Y)
+	{
+	    if(X<Y)
+	    {
+	    Y=Y-X;
+	    Y=Y- 0.50;
+	    cout<<setprecision(2)<<fixed<<Y;
+	    }
+	}
+	else
+	{
+	   cout<<setprecision(2)<<fixed<<Y;
+	}
+	return 0;
+}
